a508cc73bc38b89d8a114661cf6942b608a219ee
rizrmdhn <rizrmdhn.work@gmail.com>
2023-08-24 21:55:50 +0800

    remove

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Helpers\ResponseFormatter;
 use App\Helpers\ValidatorHelper;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
